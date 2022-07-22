7a6773d0650e6307f0ddb987f9a5374e328e13b8
Sogla <soglahash@gmail.com>
2022-07-23 01:38:06 +0200

    Add StateMachine logic

--- Scripts/StateMachine/SimpleState.cs
+++ Scripts/StateMachine/SimpleState.cs
@@ -1,21 +1,25 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public class SimpleState : Node
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+public class SimpleState : Node{
+    private bool hasBeenInitialized = false;
+    private bool onUpdateHasFired = false;
+    // State Events
+    [Signal] public delegate void StateStart();
+    [Signal] public delegate void StateUpdated();
+    [Signal] public delegate void StateExited();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+    public virtual void OnStart(Dictionary<string, object> message){
+        EmitSignal(nameof(StateStart));
+        hasBeenInitialized = true;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public virtual void OnUpdate(){
+        if (!hasBeenInitialized){
+            return;
+        }
+        EmitSignal(nameof(StateUpdated));
+        onUpdateHasFired = true;
+    }
 }
--- Scripts/StateMachine/SimpleStateMachine.cs
+++ Scripts/StateMachine/SimpleStateMachine.cs
@@ -1,21 +1,60 @@
 using Godot;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class SimpleStateMachine : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    [Signal] public delegate void PreStart();
+    [Signal] public delegate void PostStart();
+    [Signal] public delegate void PreExit();
+    [Signal] public delegate void PostExit();
+    public List<SimpleState> States;
+    public string CurrenState;
+    public string LastState;
+    protected SimpleState state = null;
     public override void _Ready()
     {
-        
+        base._Ready();
+
+        States = GetNode<Node>("States").GetChildren().OfType<SimpleState>().ToList();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void SetState(SimpleState _state, Dictionary<string, object> message)
+    {
+        if (_state == null)
+        {
+            return;
+        }
+        if (state != null){
+            EmitSignal(nameof(PreExit));
+            state.OnExit(_state.GetType().ToString());
+            EmitSignal(nameof(PostExit));
+        }
+
+        LastState = CurrenState;
+        CurrenState = _state.GetType().ToString();
+
+        state = _state;
+        EmitSignal(nameof(PreStart));
+        state.OnStart(message);
+        EmitSignal(nameof(PostStart));
+        state.OnUpdate();
+    }
+
+    public void ChangeState(string stateName, Dictionary<string, object> message = null){
+        foreach(SimpleState _state in States){
+            if(stateName == _state.GetType().ToString()){
+                SetState(_state, message);
+                return;
+            }
+        }
+    }
+
+    public override void _PhysicsProcess(float delta){
+        if(state == null){
+            return;
+        }
+        state.Updatestate(delta);
+    }
 }
